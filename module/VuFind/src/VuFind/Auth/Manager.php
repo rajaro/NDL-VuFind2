4e01481b5a6c0cf6374b2c3b57a58d8f4697ca42
rajaro <jaro.ravila@gmail.com>
2023-06-02 15:22:07 +0300

    remove test stuff

--- module/VuFind/src/VuFind/Auth/Manager.php
+++ module/VuFind/src/VuFind/Auth/Manager.php
@@ -574,9 +574,7 @@ class Manager implements
                 $this->currentUser = $results;
             } elseif ($this->cookieManager->get('loginToken')) {
                 if ($user = $this->loginToken->tokenLogin($this->sessionManager->getId())) {
-                    $map = ['database' => 'Database', 'multiils' => 'MultiILS'];
-                    $this->setAuthMethod($map[$user->auth_method]);
-                    var_dump($this->activeAuth);
+                    $this->setAuthMethod($this->getAuthMethodName($user->auth_method));
                     $this->updateUser($user);
                     $this->updateSession($user);
                 }
