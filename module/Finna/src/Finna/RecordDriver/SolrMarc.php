313b6a074ab99d5b08304ebd78e8f04fd30d28cc
Ravila <jaro.ravila@gmail.com>
2025-05-27 14:18:44 +0300

    Check 856 subfield z for image type

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -293,8 +293,10 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc implements \Laminas\Log\Log
                 $part = $this->getSubfield($url, '3');
                 // Only take large image. Thumbnail is too small.
                 $image = strcasecmp($part, 'Image') === 0;
+                if (!$image) {
+                   $image = 'Kansikuva' === $this->getSubfield($url, 'z');
+                }
             }
-
             if (
                 ($image || $pdf)
                 && $this->urlAllowed($address)
