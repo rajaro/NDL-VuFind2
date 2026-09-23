2720201a836bb86d94fcde56c8c4b885d6618615
rajaro <jaro.ravila@gmail.com>
2026-09-23 10:18:22 +0300

    [FINNA-3760] Marc: Add series title and author dates to series search link from field 800 (#3554)
    Co-authored-by: Ere Maijala <ere.maijala@helsinki.fi>

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -2170,10 +2170,20 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc implements \Psr\Log\LoggerA
                         false
                     );
                     if ($name) {
-                        $currentArray = [
-                            'name' =>
-                                $this->stripTrailingPunctuation(array_shift($name)),
-                        ];
+                        // Field 800: Build series name from author, date, and series title
+                        // instead of the first marc field, which is author in 800
+                        if ($field == '800') {
+                            $nameSubfields = ['a', 'd', 't'];
+                            $name800 = $this->getSubfieldArray($currentField, $nameSubfields, false);
+                            $name = array_diff($name, $name800);
+                            $currentArray = [
+                                'name' => $this->stripTrailingPunctuation(implode(' ', $name800)),
+                            ];
+                        } else {
+                            $currentArray = [
+                                'name' => $this->stripTrailingPunctuation(array_shift($name)),
+                            ];
+                        }
                         $currentArray['additional'] = implode(' ', $name);
 
                         // Can we find an ISSN in subfield x? (Note that ISSN is
